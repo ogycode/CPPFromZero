d71b9ec4420e5c5637560a60eeaefde3d8090017
ogycode <verlokav@gmail.com>
2017-06-06 09:38:48 +0300

    Updated: 007.Pointer

--- src/007.Pointer/007.Pointer.cpp
+++ src/007.Pointer/007.Pointer.cpp
@@ -2,69 +2,37 @@
 
 using namespace std;
 
+int Sum(int, int);
+
 int main()
 {
 	system("color 70");
 	setlocale(0, "");
 	SetConsoleTitle("007.Pointer");
 
-	int *p1 = new int;
-	int *p2 = new int(8);
-	int *p3 = new int(8);
+	int value = 10;
+	int *pointer = &value;
 
-	cout << "Non-init pointer" << endl;
-	cout << "p1=\t" << p1 << endl;
-	cout << "&p1=\t" << &p1 << "\t(address of p1)" << endl;
-	cout << "*p1=\t" << *p1 << "\t\t(value of p1)" << endl << endl;
+	cout << "value = " << value << endl;					//переменнна€
+	cout << "*pointer = " << *pointer << endl;				//значение переменной value через указатель
+	cout << "&value = " << &value << endl;					//аддрес переменной value
+	cout << "pointer = " << pointer << endl << endl;		//аддрес переменной value, значение переменной pointer
 
-	cout << "Init pointer" << endl;
-	cout << "p2=\t" << p2 << endl;
-	cout << "&p2=\t" << &p2 << "\t(address of p2)" << endl;
-	cout << "*p2=\t" << *p2 << "\t\t\t(value of p2)" << endl << endl;
+	value += 20;
+	cout << "value += 20, value = " << value << endl;
+	*pointer += 20;											//изменение значение переменной через указатель
+	cout << "*pointer += 20, value = " << value << endl << endl;
 
-	cout << "After operation:" << endl;
-	*p1 = 4;
-	cout << "p1=\t" << p1 << endl;
-	cout << "&p1=\t" << &p1 << "\t(address of p1)" << endl;
-	cout << "*p1=\t" << *p1 << "\t\t\t(value of p1)" << endl << endl;
-
-	*p3 = *p1 + *p2;
-	cout << "*p3 = *p1+*p2 = " << *p3 << endl;
-	cout << "p1=\t" << p1 << endl;
-	cout << "&p1=\t" << &p1 << "\t(address of p1)" << endl;
-	cout << "*p1=\t" << *p1 << "\t\t\t(value of p1)" << endl << endl;
-	cout << "p2=\t" << p2 << endl;
-	cout << "&p2=\t" << &p2 << "\t(address of p2)" << endl;
-	cout << "*p2=\t" << *p2 << "\t\t\t(value of p2)" << endl << endl;
-
-	cout << "Value and PValue:" << endl;
-	int value = 7;
-	int *pvalue = &value;
-
-	cout << "value=\t" << value << endl;
-	cout << "pvalue=\t" << pvalue << endl;
-	cout << "*pvalue=\t" << *pvalue << endl;
-	cout << "&pvalue=\t" << &pvalue << endl << endl;
-
-	cout << "After actions (pvalue += 1):" << endl;
-	pvalue += 1;										//work with pointer
-	cout << "pvalue=\t\t" << pvalue << endl;
-	cout << "*pvalue=\t" << *pvalue << endl;
-	cout << "&pvalue=\t" << &pvalue << endl;
-	pvalue -= 1;
-
-	cout << "After actions (*pvalue += 1):" << endl;
-	*pvalue += 1;										//work with value
-	cout << "pvalue=\t\t" << pvalue << endl;
-	cout << "*pvalue=\t" << *pvalue << endl;
-	cout << "&pvalue=\t" << &pvalue << endl;
-
-	delete p1;
-	delete p2;
-	delete p3;
-	delete pvalue;
+	//”казатель на функцию
+	int(*ptrSum)(int, int) = Sum;
+	cout << "Sum(1,1) = " << Sum(1, 1) << endl;
+	cout << "ptrSum(1,1) = " << ptrSum(1, 1) << endl;
 
 	system("pause");
-    return 0;
+	return 0;
 }
 
+int Sum(int a, int b)
+{
+	return a + b;
+}
